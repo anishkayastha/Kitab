2391a0b87a8d495d9bf11eace4cb66fa238b3fba
anishkayastha <74639382+anishkayastha@users.noreply.github.com>
2022-12-12 16:43:52 +0545

    update

--- Kitab/Program.cs
+++ Kitab/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using Kitab.Data;
+using Kitab.Data.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<KitabDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("KitabDbContext") ?? throw new InvalidOperationException("Connection string 'KitabDbContext' not found.")));
 
+//Services configuration
+builder.Services.AddScoped<IAuthorService, AuthorService>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
