1e2b051d5f62254a899f41ea714b0ca07b1cafc8
anishkayastha <74639382+anishkayastha@users.noreply.github.com>
2022-12-14 16:15:48 +0545

    more changes

--- Kitab/Models/Author.cs
+++ Kitab/Models/Author.cs
@@ -9,10 +9,17 @@ namespace Kitab.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile is required")]
         public string ProfilePictureURL { get; set; }
+
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
         public string FullName { get; set; }
+
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Biography must be less than 100 chars")]
         public string Bio { get; set; }
 
         //Relationships
