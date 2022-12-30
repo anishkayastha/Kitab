5d8ee99c8feb8d246d6b562236826125db27ab11
anishkayastha <74639382+anishkayastha@users.noreply.github.com>
2022-12-30 16:50:02 +0545

    changes

--- Kitab/Controllers/AccountController.cs
+++ Kitab/Controllers/AccountController.cs
@@ -20,5 +20,29 @@ namespace Kitab.Controllers
             _context = context;
         }
         public IActionResult Login() => View(new LoginVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Book");
+                    }
+                }
+            }
+
+            TempData["Error"] = "Wrong credentials. Please, try again!";
+            return View(loginVM);
+        }
     }
 }
